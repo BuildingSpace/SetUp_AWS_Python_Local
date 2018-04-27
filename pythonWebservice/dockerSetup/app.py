08959af58b8ba13c9636e0113f340506158061da
Marouan Boukli <marouan.boukli@zalando.de>
2018-04-27 16:53:38 +0200

    modulize python

---
+++ pythonWebservice/dockerSetup/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+from  helloWorld import app
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=80)
